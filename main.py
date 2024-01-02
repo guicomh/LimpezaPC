15c815ee4cded14cf34d396f91485117b84f4c92
Guilherme <eliseuvasconcellos@gmail.com>
2024-01-02 01:16:43 -0300

    update

--- main.py
+++ main.py
@@ -8,14 +8,15 @@ def limpar_temp():
 
 janela = Tk()
 
-janela.grid(column=0, row=1, padx= 800, pady= 600)
-
+janela.iconbitmap("icone.ico")
 janela.title("Clean Magico")
 janela.geometry("800x600")
 fonte_personalizada = ("Arial", 20, "bold")
 
+
+
 texto = Label(janela, text="CLEAN MAGICO 1.0", font=fonte_personalizada) . pack(pady=30)
 
-botão_limpeza = Button(janela, text="Limpar", command=limpar_temp, font=("Arial", 12, "bold")). pack(pady=20)
+botão_limpeza = Button(janela, text="Limpar", command=limpar_temp, font=("Arial", 12, "bold")) . pack(pady=20)
 
 janela.mainloop()
