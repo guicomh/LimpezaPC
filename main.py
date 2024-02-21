a293cc9b624f23a1343e65ee0942247c7173c096
Guilherme <eliseuvasconcellos@gmail.com>
2024-02-21 14:59:50 -0300

    update

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 import os
 from tkinter import *
-<<<<<<< HEAD
 from tkinter import ttk
-=======
-from tkinter import tkk
->>>>>>> 93de9254147a49881ac5c7628c2da3f07a7ff0e3
-
+from PIL import Image, ImageTk
 
 def limpar_temp():
-    os.system("rd/s/q %temp%")
-    os.system("cd .. && cd .. & cd.. && cd.. && cd .. && cd .. && cd .. && cd Windows && rd/s/q temp")
+    os.system("rd/s/q %temp%") 
 
 janela = Tk()
 
-janela.iconbitmap(r"C:\Users\elise\OneDrive\Documentos\GitHub\LimpezaPC\icone.ico")
-janela.title("Clean Magico")
+janela.resizable(False, False)
+
+imagem_fundo = Image.open("fundo.png")
+janela.iconbitmap("icone.ico")
+janela.title("Limpeza Pc")
 janela.geometry("800x600")
 fonte_personalizada = ("Arial", 20, "bold")
+fonte_texto = ("Arial", 14)
 
 
 
-texto = Label(janela, text="CLEAN MAGICO 1.0", font=fonte_personalizada) . pack(pady=30)
-
-botão_limpeza = Button(janela, text="Limpar", command=limpar_temp, font=("Arial", 12, "bold")) . pack(pady=20)
+botão_limpeza = Button(janela, text=" Limpeza ", command=limpar_temp, font=("Arial", 12, "bold")) . pack()
 
 
 janela.mainloop()
